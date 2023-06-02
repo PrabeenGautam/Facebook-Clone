a788dbfa30a575f045f801dc0b0f1c755669cc97
PrabinGautam <prabeen122@gmail.com>
2023-06-02 12:47:49 +0545

    Remove private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
 import { Route } from "react-router-dom";
-import PrivateRoute from "./components/routes/PrivateRoute";
 import LoginPage from "./pages/auth/Login.page";
 
 function App() {
-  const [authenticated, setAuthenticated] = useState();
-
   return (
     <>
-      <Route path="/" element={<LoginPage />} />
-      <PrivateRoute />
+      <Route path="/login" element={<LoginPage />} />
     </>
   );
 }
--- src/components/routes/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Route } from 'react-router-dom';
-
-function PrivateRoute() {
-  return ( 
-    <Route></Route>
-   );
-}
-
-export default PrivateRoute;
