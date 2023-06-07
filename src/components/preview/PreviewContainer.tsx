1465e8a62ba5f7015e6e9448841a00e50829d0a4
PrabinGautam <prabeen122@gmail.com>
2023-06-07 20:16:27 +0545

    Added File Preview

--- src/components/preview/PreviewContainer.tsx
+++ src/components/preview/PreviewContainer.tsx
@@ -2,16 +2,28 @@ import { UploadedFiles } from "@/types/component/post.types";
 import getFileBase64 from "@/utils/getBase64";
 
 function PreviewContainer({ files }: { files: UploadedFiles[] }) {
+  const renderedFiles = Array.from(files).slice(0, 4);
+  const renderedLength = renderedFiles.length;
+  const remainFile = files.length - renderedLength;
+  const isEven = files.length % 2 === 0;
+
   return (
     <>
-      {files.map((data, id) => {
+      {renderedFiles.map((data, id) => {
         return (
-          <div key={id} className="break-inside-avoid">
+          <div
+            key={id}
+            className={`relative ${
+              id === renderedLength - 1 &&
+              remainFile > 0 &&
+              "before:absolute before:inset-0 before:bg-[--dark-overlay]"
+            } ${!isEven && id === renderedLength - 1 && "col-span-2"}`}
+          >
             {data.type === "image" && (
               <img
                 src={getFileBase64(data.file)}
                 alt={`file-${id}`}
-                className="h-full w-full"
+                className="h-full w-full object-cover"
               />
             )}
 
--- src/components/preview/UploadImagePreview.tsx
+++ src/components/preview/UploadImagePreview.tsx
@@ -1,4 +1,3 @@
-import getFileBase64 from "@/utils/getBase64";
 import CloseButton from "../buttons/CloseButton";
 import { UploadedFiles } from "@/types/component/post.types";
 import PreviewContainer from "./PreviewContainer";
@@ -9,11 +8,10 @@ interface UploadImagePreview {
 }
 
 function UploadImagePreview({ onClose, files }: UploadImagePreview) {
-  const renderedFiles = Array.from(files).slice(0, 4);
   return (
     <div className="relative mt-3 flex items-center justify-between rounded-lg border border-[--divider] p-2">
-      <div className="columns-2 gap-2 space-y-2">
-        <PreviewContainer files={renderedFiles} />
+      <div className="grid grid-cols-2 gap-2 ">
+        <PreviewContainer files={files} />
       </div>
       <CloseButton className="absolute right-4 top-4" onClose={onClose} />
     </div>
