f88c5955c0ab5c5a922ec7005385405c88760c75
PrabinGautam <prabeen122@gmail.com>
2023-06-07 14:12:29 +0545

    Added Image Preview

--- src/components/images/UploadImagePreview.tsx
+++ src/components/images/UploadImagePreview.tsx
@@ -1,18 +1,27 @@
 import getFileBase64 from "@/utils/getBase64";
 import CloseButton from "../buttons/CloseButton";
-import { Files } from "@/types/data/files.types";
+import { UploadedFiles } from "@/types/component/post.types";
 
 interface UploadImagePreview {
   onClose: () => void;
-  files: Files;
+  files: UploadedFiles[];
 }
 
 function UploadImagePreview({ onClose, files }: UploadImagePreview) {
+  const renderedFiles = Array.from(files).slice(0, 4);
   return (
     <div className="relative mt-3 flex items-center justify-between rounded-lg border border-[--divider] p-2">
-      <div>
-        {Array.from(files).map((file, id) => {
-          return <img src={getFileBase64(file)} alt={`file-${id}`} />;
+      <div className="columns-2 gap-2 space-y-2">
+        {renderedFiles.map((data, id) => {
+          return (
+            <div key={id} className="aspect-auto break-inside-avoid">
+              <img
+                src={getFileBase64(data.file)}
+                alt={`file-${id}`}
+                className="h-full w-full"
+              />
+            </div>
+          );
         })}
       </div>
       <CloseButton className="absolute right-4 top-4" onClose={onClose} />
